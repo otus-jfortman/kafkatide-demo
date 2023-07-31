95942d71debab8ed0d8d60770c55ef9060c1fc7c
Joseph Fortman <joe.fortman@otus.com>
2023-07-31 15:01:15 -0500

    kafkatide demo consumer

--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
 import KafkaTide from 'kafkatide'
 
-const { consume, produce } = new KafkaTide({brokers: []})
+const { consume, produce } = new KafkaTide({
+  brokers: ['localhost:29092'],
+  clientId: 'kafkatide-demo'
+})
+
+const topic = 'a.kafkatide.demo'
+
+const { message$ } = consume({
+  topic,
+  config: {
+    groupId: 'kafkatide-demo',
+    allowAutoTopicCreation: true
+  },
+})
+
+console.log(`Subscribing to topic: ${topic}`)
+message$.subscribe((m) => {
+  console.log(`received: ${m.value}`)
+  m.workComplete()
+})
